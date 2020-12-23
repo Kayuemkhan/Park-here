71d66a5e89a8681773b5e8ccf2a6f12ed7c22d12
Kayuemkhan <abdulkayuem007@gmail.com>
2020-12-23 09:34:28 +0600

    "1.1"

--- app/src/main/java/code/fortomorrow/parkhere/ChittagongPlacesActivity.java
+++ app/src/main/java/code/fortomorrow/parkhere/ChittagongPlacesActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ChittagongPlacesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chittagong_places);
-
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/code/fortomorrow/parkhere/RajshahiPlacesActivity.java
+++ app/src/main/java/code/fortomorrow/parkhere/RajshahiPlacesActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class RajshahiPlacesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rajshahi_places);
+        ButterKnife.bind(this);
     }
 }
